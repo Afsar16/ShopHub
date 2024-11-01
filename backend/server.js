c07238ee627e692f0c8924273d3daef67ae0bdb6
Afsar16 <afsar.m1606@gmail.com>
2024-11-01 17:07:04 +0530

    product route added

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 
 import { connectDB } from "./lib/db.js";
 import authoRoutes from "./routes/auth.route.js";
+import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
 
@@ -13,7 +14,9 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+
 app.use("/api/auth", authoRoutes);
+app.use("/api/products", productRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is running on http://localhost:" + PORT);
